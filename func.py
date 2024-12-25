d05909963fbff7fbe26f6c68379158fb0b16d5c9
Ye33 <101177084+misha275@users.noreply.github.com>
2024-12-25 10:50:36 +0300

    Update func.py

--- func.py
+++ func.py
@@ -5,3 +5,15 @@ def razb(a):
     for i in range(len(b)):
         A.append(b[i])
     return A
+
+
+
+# перевод из числа из системы счисления x в систему счисления n
+def per(sch, ss1, ss2):
+    vr1 = int(str(sch), ss1)
+    vr2 = ''
+    while vr1 != 0:
+        vr2 += str(vr1%ss2)
+        vr1 //= int(ss2)
+    vr2 = vr2[::-1]
+    return vr2
